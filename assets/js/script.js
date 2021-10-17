cd3a3cb7ca794cc9350cf398703dc418d2b66eeb
Joel Afamefune <jafamefune@gmail.com>
2021-10-17 11:38:11 -0500

    Set click listeners on projects.

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+$("#weather-app").click(function(){
+    window.location = "https://github.com/Joe96/openweather_api";
+});
+
+$("#geography-test").click(function(){
+    window.location = "https://github.com/Joe96/code-quiz";
+});
+
+$("#password-generator").click(function(){
+    window.location = "https://github.com/Joe96/password-generator";
+});
+
+$("#day-planner").click(function(){
+    window.location = "https://github.com/Joe96/day-planner";
+});
+
+$("#translator").click(function(){
+    window.location = "https://github.com/Joe96/bantunese";
+});
+
+$("#pong").click(function(){
+    window.location = "https://github.com/Joe96/projectpong";
+});
